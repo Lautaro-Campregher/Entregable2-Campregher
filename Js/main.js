02274d796d3b0b915834f9a58171526aa86887f9
Lautaro Campregher <lautaronicolas201422@gmail.com>
2025-09-22 15:00:45 -0300

    Simulador finalizado

--- Js/main.js
+++ Js/main.js
@@ -82,7 +82,7 @@ const agregarAlCarrito = (id) => {
 // Cargar productos desde el archivo JSON
 const cargarProductos = async () => {
   try {
-    const response = await axios.get("./data/data.json");
+    const response = await axios.get("./public/data.json");
     productos = response.data;
     mostrarCatalogo(productos);
   } catch (error) {
